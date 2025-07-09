576d82a1aab4ccf6b737b8cac45f4ee89f78adbb
DeivisMon <deividosius@yahoo.com>
2025-07-09 22:32:27 +0300

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route, useLocation } from "react-router-dom"
+import Index from "./pages/Index"
+import Work from "./pages/Work"
+import Contact from "./pages/Contact"
+import { AnimatePresence } from "framer-motion"
+import PageTransitions from "./components/PageTransitions"
+
+export default function App() {
+  const location = useLocation()
+
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        <PageTransitions key={location.pathname}>
+          <Routes location={location}>
+            <Route path="/" element={<Index />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </PageTransitions>
+      </AnimatePresence>
+    </>
+  )
+}
