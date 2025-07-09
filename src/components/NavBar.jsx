27b64e9082f6dcb30df9eb3fbbbc413d57274666
DeivisMon <deividosius@yahoo.com>
2025-07-09 22:34:59 +0300

    page transitions, navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom"
+import AnimatedText from "../utils/AnimatedText"
+
+export default function NavBar() {
+  return (
+    <div className="fixed top-0 left-0 w-full bg-black/80 nav flex items-center justify-between px-6 py-4 z-1000 ">
+      <div className="logo text-lg font-medium">
+        <Link to="/">
+          <AnimatedText text="Darius Zvinklys"  />
+        </Link>
+      </div>
+      <div className="nav-links flex space-x-6 font-medium text-sm">
+        <Link to="/">
+          <AnimatedText text="Index" duration={0.3} />
+        </Link>
+        <Link to="/work">
+          <AnimatedText text="Work" duration={0.4} />
+        </Link>
+        <Link to="/contact">
+          <AnimatedText text="Contact" duration={0.5} />
+        </Link>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/PageTransitions.jsx
@@ -0,0 +1,83 @@
+import { motion as M } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const blockDelay = (rowIndex, totalRows) => {
+  const blockDelay = Math.random() * 0.25;
+  const rowDelay = (totalRows - rowIndex - 1) * 0.05;
+  return blockDelay + rowDelay;
+};
+
+export default function PageTransitions({ children }) {
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIsAnimating(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="relative">
+      {/* Transition-in (will animate on enter) */}
+      {isAnimating && (
+        <div className="blocks-container transition-in fixed top-0 left-0 w-full h-full flex flex-col pointer-events-none z-100">
+          {Array.from({ length: 8 }).map((_, rowIndex) => (
+            <div key={rowIndex} className="row flex flex-1 w-full ">
+              {Array.from({ length: 16 }).map((_, blockIndex) => (
+                <M.div
+                  key={blockIndex}
+                  className="block relative flex-1 bg-black/90 m-[-0.25px] origin-top inset-shadow-xl"
+                  initial={{ scaleY: 1, rotateX: 0 }}
+                  animate={{ scaleY: 0, rotateX: 360 }}
+                  transition={{
+                    duration: 0.75,
+                    ease: "easeInOut",
+                    delay: blockDelay(rowIndex, 8),
+                  }}
+                />
+              ))}
+            </div>
+          ))}
+        </div>
+      )}
+
+      <M.div
+        className="relative z-10"
+        // initial={{ y: -100, opacity: 0 }}
+        // animate={{
+        //   y: 0,
+        //   opacity: 1,
+        //   transition: { duration: 0.25, delay: 0.5 },
+        // }}
+        // exit={{
+        //   y: -100,
+        //   opacity: 0,
+        //   transition: { duration: 0.25, delay: 0.5 },
+        // }}
+      >
+        {children}
+      </M.div>
+
+      {/* Transition-out (will animate on exit) */}
+      <div className="blocks-container transition-out fixed top-0 left-0 w-full h-full flex flex-col pointer-events-none z-100">
+        {Array.from({ length: 8 }).map((_, rowIndex) => (
+          <div key={rowIndex} className="row flex flex-1 w-full">
+            {Array.from({ length: 16 }).map((_, blockIndex) => (
+              <M.div
+                key={blockIndex}
+                className="block relative flex-1 bg-black/90 m-[-0.25px] origin-bottom inset-shadow-xl"
+                initial={{ scaleY: 0, rotateX: -360 }}
+                animate={{ scaleY: 0, rotateX: -360 }}
+                exit={{ scaleY: 1, rotateX: 0 }}
+                transition={{
+                  duration: 0.75,
+                  ease: "easeInOut",
+                  delay: blockDelay(rowIndex, 8),
+                }}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
